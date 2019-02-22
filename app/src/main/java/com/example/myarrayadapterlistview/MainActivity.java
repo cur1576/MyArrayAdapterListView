b7095e2846b6afbab96992f82b8d2133dae18b0e
cur1576@gmx.de <8sje7wksksK9wure>
2019-02-22 11:10:02 +0100

    version01

--- app/src/main/java/com/example/myarrayadapterlistview/MainActivity.java
+++ app/src/main/java/com/example/myarrayadapterlistview/MainActivity.java
@@ -3,11 +3,18 @@ package com.example.myarrayadapterlistview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<MyItem> items = new ArrayList<>();
+        items.add(new MyItem("Christian", "aus Berlin",R.mipmap.ic_launcher,true));
+        items.add(new MyItem("Frank", "aus Berlin",R.mipmap.ic_launcher_round,false));
     }
 }

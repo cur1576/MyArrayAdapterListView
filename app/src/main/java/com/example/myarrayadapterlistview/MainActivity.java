3cf6aa5f98454c38321d63cc76e954bfd1e27fac
cur1576@gmx.de <8sje7wksksK9wure>
2019-02-22 11:15:36 +0100

    version02

--- app/src/main/java/com/example/myarrayadapterlistview/MainActivity.java
+++ app/src/main/java/com/example/myarrayadapterlistview/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myarrayadapterlistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
         List<MyItem> items = new ArrayList<>();
         items.add(new MyItem("Christian", "aus Berlin",R.mipmap.ic_launcher,true));
         items.add(new MyItem("Frank", "aus Berlin",R.mipmap.ic_launcher_round,false));
+        items.add(new MyItem("Ronny", "aus Berlin",R.mipmap.ic_launcher_round,true));
+        items.add(new MyItem("Sebastien", "aus Offenbach",R.mipmap.ic_launcher,true));
+
+        ListView listView = findViewById(R.id.lv_mylist);
+
+        View header = getLayoutInflater().inflate(R.layout.listview_header,null);
+        MyAdapter adapter = new MyAdapter(this,R.layout.listview_item,items);
+        listView.addHeaderView(header);
+        listView.setAdapter(adapter);
     }
 }
